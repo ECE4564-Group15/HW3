d69666e78674c0f08ca3a7eae73a587d6e38203f
Beichen <lbc0430@gmail.com>
2017-03-18 02:59:37 -0400

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+###############################################################################
+# This script installs coresponding python library, including
+# googlemaps -> convert the zip to latitude and longitude
+# ephem -> performes high-precision astronomy computations
+# twilio -> send SMS
+# pygame -> play music
+# space track account: bcliu430@vt.edu
+#             passwd:  BeichenL960430!
+# teilio account: bcliu430@vt.edu
+#        passwd:  BeichenL960430		
+#
+#
+sudo pip install googlemaps
+sudo pip install ephem
+sudo pip install twilio
+sudo pip install pygame
